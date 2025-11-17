0baeb8087dd9087ca11522ecccf35c338924e151
olyad boka <olyadboka@gmail.com>
2025-11-17 17:01:51 +0300

    added the other end point in node js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,13 +1,30 @@
-const http = require("node:http");
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application / json" });
-  res.end(
-    JSON.stringify({
-      message: "Welcome to out first nodej API ",
-      email: "olyadboka@gmail.com",
-    })
-  );
+  console.log("The method is:" + req.method);
+  console.log("The url is:" + req.url);
+  console.log("The headers are:" + req.headers);
+
+  if (req.url == "/api/users") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify([
+        { id: 1, name: "olyad" },
+        { id: 2, name: "Bati" },
+        { id: 3, name: "Alazer" },
+      ])
+    );
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Endpoint not found" }));
+  }
+  // res.writeHead(200, { "Content-Type": "application / json" });
+  // res.end(
+  //   JSON.stringify({
+  //     message: "Welcome to out first nodej API ",
+  //     email: "olyadboka@gmail.com",
+  //   })
+  // );
 });
 
 const PORT = 3000;
