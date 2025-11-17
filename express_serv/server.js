b1eb29ef20bf69b8c0907854ff1d42712dd5740b
olyad boka <olyadboka@gmail.com>
2025-11-17 17:52:27 +0300

    added some labiraries like the nodemon and express

--- express_serv/server.js
+++ express_serv/server.js
@@ -8,6 +8,35 @@ let users = [
   { id: 3, name: "Kumsa" },
 ];
 
-app.run(3000, () => {
+//CREATE a new user
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+//READ all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+//UPDATE  a user by ID
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updateUser = req.body;
+  users = users.map((user) =>
+    user.id == userId ? { ...user, ...updateUser } : user
+  );
+  res.json(updateUser);
+});
+
+//DELETE a user by ID
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id != id);
+  res.status(204).send();
+});
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
