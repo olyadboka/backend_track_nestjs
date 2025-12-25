9856fdc8d09a28a8e9ea1b35e5619314bb479a52
Olyad Boka <olyadboka@gmail.com>
2025-12-25 16:49:39 +0300

    added transaction schema updated with export the transaction schema

--- chella-api/src/transactions/schemas/transactions.schema.ts
+++ chella-api/src/transactions/schemas/transactions.schema.ts
@@ -1,22 +1,25 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 
 @Schema({timestamps: true})
 
 export class Transaction extends Document{
-@Prop({ required: true })
-//  transferId: Types.ObjectId()
-senderId: string
-@Prop({ required: true })
-receiverId: string
+          @Prop({ required: true })
+          //  transferId: Types.ObjectId()
+          senderId: string
+          @Prop({ required: true })
+          receiverId: string
 
-@Prop({ required: true })
-amount: number
-@Prop({ default:false })
-
-isCompleted: boolean
-@Prop({default: "ETB"})
-currency: string
+          @Prop({ required: true })
+          amount: number
+          @Prop({ required: true })
+          status: string
+          @Prop({default: "ETB"})
+          currency: string
 
 
 }
+
+
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction)
